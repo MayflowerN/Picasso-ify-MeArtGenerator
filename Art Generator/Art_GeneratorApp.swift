5141d0a59fe452287d3b6c4690a9b129b648cd8e
EllieNguyen <ellieN1235@gmail.com>
2023-06-19 22:13:26 -0700

    Initial Commit

---
+++ Art Generator/Art_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_GeneratorApp.swift
+//  Art Generator
+//
+//  Created by Ellie on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Art_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
